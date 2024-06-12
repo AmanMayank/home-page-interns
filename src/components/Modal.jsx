8b12de918b0d8342172c70e4d7cccba777385937
Aman Mayank <62825517+AmanMayank@users.noreply.github.com>
2024-06-12 19:17:18 +0530

    Updated Modal

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,14 +1,28 @@
 import React from "react";
+import { AiFillCloseSquare } from "react-icons/ai";
+import { HiOutlineArrowNarrowRight } from "react-icons/hi";
 
 const Modal = ({ isOpen, onClose }) => {
   return (
     <>
       {isOpen && (
         <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50">
-          <div className=" bg-[#0f0e11] p-2 w-[50%]">
-            <h2 className="text-xl font-bold mb-4 text-white">
-              Let's discuss the Project
-            </h2>
+          <div className=" bg-[#0f0e11] p-2 w-[65%] lg:min-w-[576px]">
+            <div className="flex justify-between items-center">
+              <div className="text-xl font-barlow-2 font-bold mb-4 text-white text-[18px] pl-[26px] pt-[25px]">
+                Let's Discuss the Project.
+              </div>
+              <div className="p-3" onClick={onClose}>
+                <AiFillCloseSquare
+                  style={{
+                    color: "white",
+                    background: "black",
+                    fontSize: "25px",
+                    top: "-40px",
+                  }}
+                />
+              </div>
+            </div>
             {/* Form for entering user details */}
             <form>
               <div className="mb-[3px] flex space-x-[3px]">
@@ -60,19 +74,13 @@ const Modal = ({ isOpen, onClose }) => {
                   className="w-full bg-[#393943]  p-2 mb-[3px]"
                 />
               </div>
-              <div className="flex justify-end">
-                <button
-                  type="button"
-                  className="bg-gray-500 text-white px-4 py-2 rounded-md mr-2"
-                  onClick={onClose}
-                >
-                  Close
-                </button>
+              <div className="flex justify-between w-[200px]">
                 <button
                   type="submit"
-                  className="bg-blue-500 text-white px-4 py-2 rounded-md"
+                  className="px-[12px] py-[16px] bg-gradient-to-r from-[#03256c] to-green-500 hover:bg-gradient-to-r hover:from-green-500 hover:to-[#03256c] text-white font-barlow font-bold  w-full flex justify-between items-center"
                 >
-                  Submit
+                  SUBMIT
+                  <HiOutlineArrowNarrowRight />
                 </button>
               </div>
             </form>
